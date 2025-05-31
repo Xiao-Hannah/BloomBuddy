cd4c1cabeb34ffd7171e3e744f402f52bd0e7431
Xiao-Hannah <hannaxiao0628@gmail.com>
2025-05-30 20:10:41 -0700

    adding deploy logic

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
+
+const nextConfig = {
+  output: "export",
+  basePath: isProd ? "/BloomBuddy" : "",
+  assetPrefix: isProd ? "/BloomBuddy/" : "",
+};
+
+module.exports = nextConfig;
